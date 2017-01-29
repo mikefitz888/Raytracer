cea4e4ae0144795a3e06e1d0310f65a86b0d858e
MishMash <mp14959@bristol.ac.uk>
2017-01-29 22:21:57 +0000

    Reverted changes to main raytracer, and moved them to texturesystem branch

--- Source/Raytracer.cpp
+++ Source/Raytracer.cpp
@@ -13,7 +13,7 @@
 #include "../Include/Raytracer.h"
 #include "../Include/ModelLoader.h"
 
-#define _DOF_ENABLE_ false
+#define _DOF_ENABLE_ true
 
 //VS14 FIX
 FILE _iob[] = { *stdin, *stdout, *stderr };
@@ -41,12 +41,12 @@ void Draw(std::vector<Triangle>& model);
 glm::vec3 Trace(float x, float y, std::vector<Triangle>& triangles, glm::vec3 cameraPos, glm::vec3 direction);
 
 int main(int argc, char** argv) {
-	/*std::cout << "wooowowowo" << std::endl;
+	std::cout << "wooowowowo" << std::endl;
 	model::Model model("../model.txt");
 	int x;
 	std::cin >> x;
-	return 0;*/
-	screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
+	return 0;
+	/*screen = InitializeSDL( SCREEN_WIDTH, SCREEN_HEIGHT );
 	t = SDL_GetTicks();	// Set start value for timer.
 
 	std::vector<Triangle> model = std::vector<Triangle>();
@@ -54,24 +54,24 @@ int main(int argc, char** argv) {
 
 	while( NoQuitMessageSDL() )
 	{
-		Update();
-		Draw(model);
+	Update();
+	Draw(model);
 	}
 
 	SDL_SaveBMP( screen, "screenshot.bmp" );
-	return 0;
+	return 0;*/
 }
 
 void Update() {
 	// Compute frame time:
 	int t2 = SDL_GetTicks();
-	float dt = float(t2-t);
+	float dt = float(t2 - t);
 	t = t2;
 	std::cout << "Render time: " << dt << " ms." << std::endl;
 }
 
 void Draw(std::vector<Triangle>& model) {
-	if( SDL_MUSTLOCK(screen) )
+	if (SDL_MUSTLOCK(screen))
 		SDL_LockSurface(screen);
 
 	float DOF_focus_length = 2.750f;
@@ -90,10 +90,8 @@ void Draw(std::vector<Triangle>& model) {
 	float x_rotation = 0.5f;
 	float y_rotation = 0.0f;
 
-	for( int y=0; y<SCREEN_HEIGHT; ++y )
-	{
-		for( int x=0; x<SCREEN_WIDTH; ++x )
-		{
+	for (int y = 0; y<SCREEN_HEIGHT; ++y) {
+		for (int x = 0; x<SCREEN_WIDTH; ++x) {
 			float xScr = (2 * (x - SCREEN_WIDTH / 2) / (float)(SCREEN_WIDTH)) * aspect_ratio * fovFactor;
 			float yScr = (2 * (y - SCREEN_HEIGHT / 2) / (float)(SCREEN_HEIGHT)) * fovFactor;
 			glm::vec3 direction(xScr, yScr, focalLength);
@@ -111,7 +109,7 @@ void Draw(std::vector<Triangle>& model) {
 						cameraClone -= focus_point; //translate such that focus_point is origin
 						cameraClone = glm::rotateX(cameraClone, xr); //rotation in X
 						cameraClone = glm::rotateY(cameraClone, yr); //rotation in Y
-						//For direction vector: give same rotation as camera
+																	 //For direction vector: give same rotation as camera
 						direction = glm::rotateX(direction, xr);
 						direction = glm::rotateY(direction, yr);
 
@@ -119,21 +117,19 @@ void Draw(std::vector<Triangle>& model) {
 						color_buffer += Trace(xScr, yScr, model, cameraClone, direction);
 					}
 				}
-				
+
 				color = color_buffer / glm::vec3((float)samples);
-			}
-			else {
+			} else {
 				color = Trace(x, y, model, cameraPos, direction);
 			}
-			PutPixelSDL( screen, x, y, color );
+			PutPixelSDL(screen, x, y, color);
 		}
-		printf("Progress: %f \n", ((float)y / (float)SCREEN_HEIGHT)*100.0f);
 	}
 
-	if( SDL_MUSTLOCK(screen) )
+	if (SDL_MUSTLOCK(screen))
 		SDL_UnlockSurface(screen);
 
-	SDL_UpdateRect( screen, 0, 0, 0, 0 );
+	SDL_UpdateRect(screen, 0, 0, 0, 0);
 }
 
 //Returns colour of nearest intersecting triangle
@@ -163,22 +159,6 @@ glm::vec3 Trace(float xScr, float yScr, std::vector<Triangle>& triangles, glm::v
 			}
 		}
 
-		// Calculate interpolated colour:
-		/*
-			TEMP (explanation of barycentric coordinates):
-				- Barycentric coordinates are an alternative way to represent a triangle as the combination of different weights.
-				How it boils down is that a coordinate on the surface of a triangle plane can be given a 3D barycentric coordinate (a, b, c). 
-
-				The position of that coordinate can then be converted to euclidean space by taking  pos = v0*a + v1*b + v2*c
-				- Conveniently, this allows us to use it to interpolate values. We can give each vertex a colour, and interpolate 
-				between them, we can also give each vertex a uv coordinate (mapping to texture space) and use this same
-				math to map each point within a triangle to the correct point in uv-space.
-		*/
-		glm::vec3 barycentric_coords = triangles[closest_intersect.index].calculateBarycentricCoordinates(closest_intersect.position);
-		
-		baseColour = glm::vec3(1.0, 0.0, 0.0)*barycentric_coords.x + glm::vec3(0.0, 1.0, 0.0)*barycentric_coords.y + glm::vec3(0.0, 0.0, 1.0)*barycentric_coords.z;
-
-		
 		return baseColour*light_factor;
 	}
 	return color_buffer;
